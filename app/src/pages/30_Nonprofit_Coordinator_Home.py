69ec205eeb2bef72bcdb0492030af7da9c508607
Alyssa Haidar <alyssa.haidar14@gmail.com>
2026-04-19 16:58:14 -0400

    add vector nonprofit coordinator home page

--- app/src/pages/30_Nonprofit_Coordinator_Home.py
+++ app/src/pages/30_Nonprofit_Coordinator_Home.py
@@ -8,10 +8,20 @@ st.set_page_config(layout='wide')
 
 SideBarLinks()
 
-st.title('System Admin Home Page')
+st.title(f"Welcome Nonprofit Coordinator, {st.session_state['first_name']}! 🌱")
 st.write('### What would you like to do today?')
 
-if st.button('Update ML Models',
-             type='primary',
-             use_container_width=True):
-    st.switch_page('pages/21_ML_Model_Mgmt.py')
+if st.button('View Food Waste Trends',
+            type='primary',
+            use_container_width=True):
+    st.switch_page('pages/31_Vector_Waste_Trends.py')
+
+if st.button('Program Participant Activity',
+            type='primary',
+            use_container_width=True):
+    st.switch_page('pages/32_Vector_Activity.py')
+
+if st.button('Payment Method Insights',
+            type='primary',
+            use_container_width=True):
+    st.switch_page('pages/33_Vector_Payment_Insights.py')
